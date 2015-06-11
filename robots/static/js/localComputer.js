c6fe0060bb0222ecf7c8c8ac99a6730b28d85577
Bruce Wootton <bruce@brightboxtech.com>
2015-06-11 13:21:39 -0700

    Validate timezone included on all objects

--- robots/static/js/localComputer.js
+++ robots/static/js/localComputer.js
@@ -3,13 +3,14 @@ function LocalComputer($scope, $routeParams,Restangular){
     $scope.saveComputer = function(){
         $scope.localComputer.save().then(function(computer){
             alert("Saved Computer");
+            $scope.loadComputer();
         }, function(reason){
             alert("Failed to Save " + reason);
         });
     };
 
     $scope.loadComputer = function(){
-        return  Restangular.one("/api/v1/local_computer", $routeParams.id).get().then(function (localComputer){
+        return  Restangular.one("local_computer", $routeParams.id).get().then(function (localComputer){
             $scope.localComputer=localComputer;
         }, function(reason){
             alert("Couldn't load localComputer: " + reason);
