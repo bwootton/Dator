2689eaa1da4effb530dabe95da14b4c8eb3c3232
Bruce Wootton <bruce@brightboxtech.com>
2015-09-02 10:51:39 -0700

    Move UI to separate app

---
+++ manage_ui/static/js/localComputer.js
@@ -0,0 +1,128 @@
+function LocalComputer($scope, $routeParams, $interval, Restangular) {
+
+    $scope.saveComputer = function () {
+        $scope.localComputer.save().then(function (computer) {
+            alert("Saved Computer");
+            $scope.getComputer();
+        }, function (reason) {
+            alert("Failed to Save " + reason);
+        });
+    };
+
+    $scope.getComputer = function () {
+        return Restangular.one("local_computer", $routeParams.id).get().then(function (localComputer) {
+            $scope.localComputer = localComputer;
+        }, function (reason) {
+            alert("Couldn't load localComputer: " + reason);
+        });
+    };
+
+    $scope.getComputerStatus = function () {
+        return Restangular.one("local_computer", $routeParams.id).get().then(function (localComputer) {
+            $scope.localComputer.is_running = localComputer.is_running;
+        }, function (reason) {
+            console.log("Couldn't load localComputer: " + reason);
+        });
+    };
+
+    $scope.getPrograms = function () {
+        return Restangular.all("program").getList().then(function (programs) {
+            $scope.programs = programs;
+        }, function (reason) {
+            alert("Couldn't load programs: " + reason)
+        });
+    };
+
+    /**
+     * Send a COMMAND_DONE to the local computer
+     */
+    $scope.stopComputer = function () {
+        var command = {};
+        command.type = COMMAND_DONE;
+        command.local_computer_id = $scope.localComputer.id;
+
+
+        return Restangular.all("command").post(command, "", {}, {}).then(function () {
+            alert("Sent shutdown to box");
+        }, function (reason) {
+            alert("Couldn't shutdown local computer: " + reason);
+        });
+    };
+
+    /**
+     * Send a COMMAND_LOAD_PROGRAM to the local computer
+     */
+    $scope.loadProgram = function (program_id) {
+        var command = {};
+        command.type = COMMAND_LOAD_PROGRAM;
+        command.local_computer_id = $scope.localComputer.id;
+        command.json_command = JSON.stringify({'program_id': program_id});
+
+
+        return Restangular.all("command").post(command, "", {}, {}).then(function () {
+            alert("Requested program start");
+        }, function (reason) {
+            alert("Couldn't start program on local computer: " + reason);
+        });
+    };
+
+    /**
+     * Send a COMMAND_STOP_PROGRAM to the local computer
+     */
+    $scope.stopProgram = function (program_id) {
+        var command = {};
+        command.type = COMMAND_STOP_PROGRAM;
+        command.local_computer_id = $scope.localComputer.id;
+        command.json_command = JSON.stringify({'program_id': program_id});
+
+
+        return Restangular.all("command").post(command, "", {}, {}).then(function () {
+            alert("Requested program stop");
+        }, function (reason) {
+            alert("Couldn't stop program on local computer: " + reason);
+        });
+    };
+
+    /**
+     * Get a list of signals associated with the computer.
+     */
+    $scope.getSignals = function () {
+        return Restangular.all("signal")
+    };
+
+    $scope.getComputer();
+    $scope.getPrograms();
+
+
+    var promise = $interval($scope.getComputerStatus, 5000);
+
+    // Cancel interval on page changes
+    $scope.$on('$destroy', function () {
+        if (angular.isDefined(promise)) {
+            $interval.cancel(promise);
+            promise = undefined;
+        }
+    });
+
+    $scope.newProgram = function(){
+        return Restangular.all("program").post({name:"new program", code:""}).then(function (created){
+            $scope.getPrograms();
+        });
+    };
+
+    $scope.deleteProgram = function(programId){
+        var program = _.find($scope.programs, function(program){
+            return program.id == programId;
+        });
+        return Restangular.one("program", program.id).get().then(function(foundProgram){
+            foundProgram.remove().then(
+                function(removeStatus){
+                    $scope.getPrograms();
+                });
+        });
+    };
+
+}
+
+angular.module('Ruenoor').controller('LocalComputer',
+    ['$scope', '$routeParams', '$interval', 'Restangular', LocalComputer]);
---
+++ manage_ui/static/js/ruenoor.js
@@ -0,0 +1,38 @@
+var app = angular.module('Ruenoor', ['restangular', 'ngRoute', 'ui.ace'])
+    .config(function (RestangularProvider, $routeProvider, $httpProvider) {
+
+        // set routes
+        $routeProvider.when('/editor/:id', {
+            templateUrl: '/static/templates/editor.html'
+        }).when('/local_computer/:id', {
+            templateUrl: '/static/templates/local_computer.html'
+        }).when('/', {
+            templateUrl: '/static/templates/main.html'
+        });
+
+        // configure restangular to work with tastypie, which returns data in an objects list, meta data in a meta object
+        RestangularProvider.setBaseUrl("/api/v1");
+
+        RestangularProvider.setResponseExtractor(function (response, operation, what, url) {
+            var newResponse;
+
+            if (operation === "getList") {
+                newResponse = response.objects;
+                newResponse.metadata = response.meta;
+            } else {
+                newResponse = response;
+            }
+
+            return newResponse;
+        });
+
+        RestangularProvider.setRequestSuffix('/?format=json');
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    });
+
+//app.config(['$httpProvider', function ($httpProvider) {
+//    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+//    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+//}]);
+
